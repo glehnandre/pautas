02c300cc3da5d8ca9a7f4f306124bd133a7c3d21
André von glehn <glehnandre@gmail.com>
2021-07-21 15:03:05 -0300

    :wrench: Adding correct Status "Vista"

--- src/app/modules/acervo/tabela/tabela.component.ts
+++ src/app/modules/acervo/tabela/tabela.component.ts
@@ -9,7 +9,7 @@ import { Paginacao } from './paginacao/paginacao.component';
 export enum SituacaoDoProcesso {
   'Apto a Julgar' = 1, 
   'Em julgamento', 
-  'Visitado', 
+  'Vista', 
   'Pautado', 
   'Suspenso', 
   'Retirado de pauta', 
