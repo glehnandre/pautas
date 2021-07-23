d68c09dca68727dba5e0c6ebadccd4efabdddf29
André von glehn <glehnandre@gmail.com>
2021-07-23 18:16:00 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/app/modules/acervo/acoes/pautar/sessoes.pipe.ts
+++ src/app/modules/acervo/acoes/pautar/sessoes.pipe.ts
@@ -6,12 +6,14 @@ import { SessaoJulgamento } from "./sessaoJulgamento";
 })
 export class SessoesPipe implements PipeTransform {
     transform(sessoes: SessaoJulgamento[], colegiado: string, dataInicio: Date, dataFim: Date) {
+        console.log('%c Data Inicio', 'color:blue; font-weight: bold; font-size: 20px;');
+        console.log({colegiado, dataInicio, dataFim });
         if (!colegiado && !dataInicio && !dataFim) {
             return sessoes;
         } else
 
             return sessoes.filter(sessao => {
-                //incluir aqui a logica para que não seja necessário que todos os filtros estejam preechidos para serem utilizados. Outra alternativa é cirar um pipe para cada tipo de atributo e concatená-los
+                //incluir aqui a logica para que não seja necessário que todos os filtros estejam preechidos para serem utilizados. Outra alternativa é cirar um pipe para cada tipo de atributo 
                     return sessao.colegiado === colegiado;
             }
             );
