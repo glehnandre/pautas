4d96adc68e6ff45e64fd213c771e89f1cca622ae
RobertoCaixeta <53282655+RobertoCaixeta@users.noreply.github.com>
2021-08-05 14:27:13 -0300

    :star: Adiciona mat-select para as tags Aplica as tags selecionadas

--- src/app/modules/acervo/filtros/filtro-dialog.component.ts
+++ src/app/modules/acervo/filtros/filtro-dialog.component.ts
@@ -3,6 +3,9 @@ import { Component, Output, EventEmitter, Inject, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Filtros } from './filtros';
+import { tags as tagData } from 'app/mock-api/pautas/tags/data';
+import { Tag } from 'app/modules/acervo/model/interfaces/tag.interface';
+
 
 @Component({
   selector: 'filtro-dialog.component',
@@ -12,9 +15,9 @@ export class FiltroDialogComponent implements OnInit {
 
   form: FormGroup;
   filtros: Filtros;
-  classesLista: string[] = ['AC','ACO','ADC','ADI','ADO','ADPF','AI','AImp','AO','AOE','AP','AR','ARE','AS','CC','Cm','EI','EL','EP','Ext','HC','HD','IF','Inq','MI','MS','Pet','PPE','PSV','C','Rcl','RE','RHC','RHD','RMI','RMS','RvC','SIRDR','SL','SS','STA','STP','TPA'];
+  classesLista: string[] = ['AC', 'ACO', 'ADC', 'ADI', 'ADO', 'ADPF', 'AI', 'AImp', 'AO', 'AOE', 'AP', 'AR', 'ARE', 'AS', 'CC', 'Cm', 'EI', 'EL', 'EP', 'Ext', 'HC', 'HD', 'IF', 'Inq', 'MI', 'MS', 'Pet', 'PPE', 'PSV', 'C', 'Rcl', 'RE', 'RHC', 'RHD', 'RMI', 'RMS', 'RvC', 'SIRDR', 'SL', 'SS', 'STA', 'STP', 'TPA'];
   situacaoLista: string[] = ['Apto a Julgar', 'Pautado', 'Retirado de pauta', 'Solicitada sessão extraordinária', 'Em julgamento', 'Julgado', 'Suspenso', 'Vista', 'Retonado de vista']
-
+  tags: Tag[] = tagData;
   /**
    * 
    * @param dialogRef Referencia para a caixa de dialogo
@@ -27,11 +30,13 @@ export class FiltroDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) data
   ) {
     this.filtros = data.filtros;
+    this.tags = tagData;
   }
 
   /**
    * Inicializa formulários
    */
+
   ngOnInit(): void {
     this.form = this.fb.group({
       filtros: this.filtros.termo,
@@ -42,6 +47,7 @@ export class FiltroDialogComponent implements OnInit {
       tags: [this.filtros.tags],
       situacoes: [this.filtros.situacoes]
     });
+
   }
 
   fechar(): void {
