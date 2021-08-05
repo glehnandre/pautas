45cb113f1ae22c536d409eb3620538f197176792
DouglasMonteles <silvadouglas328@gmail.com>
2021-08-05 14:01:05 -0300

    :speakles: Add os chips

--- src/app/modules/julgamento-extraordinario/julgamento-extraordinario.module.ts
+++ src/app/modules/julgamento-extraordinario/julgamento-extraordinario.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { JulgamentoExtraordinarioComponent } from './julgamento-extraordinario.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const JulgamentoExtraordinarioRoutes: Route[] = [
   { path: '', component: JulgamentoExtraordinarioComponent, pathMatch: 'full' },
@@ -14,6 +17,9 @@ const JulgamentoExtraordinarioRoutes: Route[] = [
   imports: [
     CommonModule,
     RouterModule.forChild(JulgamentoExtraordinarioRoutes),
+    MatChipsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [
     JulgamentoExtraordinarioComponent,
