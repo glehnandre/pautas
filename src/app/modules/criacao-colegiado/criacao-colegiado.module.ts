3c5fe786605a95075a42ce787517bca776c26130
DouglasMonteles <silvadouglas328@gmail.com>
2021-08-10 13:50:04 -0300

    :speakles: Criando o colegiado e add a estrutura

--- src/app/modules/criacao-colegiado/criacao-colegiado.module.ts
+++ src/app/modules/criacao-colegiado/criacao-colegiado.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CriacaoColegiadoComponent } from './criacao-colegiado.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const CriacaoColegiadoRoutes: Routes = [
   { path: '', component: CriacaoColegiadoComponent },
@@ -14,6 +18,11 @@ const CriacaoColegiadoRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(CriacaoColegiadoRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   exports: [
     CriacaoColegiadoComponent,
