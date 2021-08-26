72ed9c6d9c98ed2b3a837e535526c63f8ec7721d
André von glehn <glehnandre@gmail.com>
2021-08-26 10:01:20 -0300

    ⭐️Cria estrutura de capítulos e decisões por capítulo

--- src/app/mock-api/pautas/julgamentos/data.ts
+++ src/app/mock-api/pautas/julgamentos/data.ts
@@ -58,7 +58,8 @@ export const processos: Processo[] = [
         id: 123455,
         descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis eum ratione, impedit, consequuntur laborum amet tempore iure accusamus in nulla voluptates odit. Eum fugit eius, assumenda distinctio quidem quia?",
         ordem: 1,
-        tipo: "Mérito"
+        tipo: "Mérito",
+        dispositivo: "Deferido"
       }
     ]
   },
@@ -103,7 +104,8 @@ export const processos: Processo[] = [
         id: 123455,
         descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis eum ratione, impedit, consequuntur laborum amet tempore iure accusamus in nulla voluptates odit. Eum fugit eius, assumenda distinctio quidem quia?",
         ordem: 1,
-        tipo: "Mérito"
+        tipo: "Mérito",
+        dispositivo: "Deferido"
       }
     ]
   },
