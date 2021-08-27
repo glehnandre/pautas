055ac5a463184af516d0e9af15a6898f853b25f8
Caio de Oliveira Mendes <caioom00@gmail.com>
2021-08-27 14:28:12 -0300

    listagem de processos e mudança de layout

--- src/app/modules/sessoes-julgamentos/sessoes-julgamentos.component.ts
+++ src/app/modules/sessoes-julgamentos/sessoes-julgamentos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Processo } from '../acervo/model/interfaces/processo.interface';
+import { ProcessoService } from '../services/processo.service';
 
 @Component({
   selector: 'app-sessoes-julgamentos',
@@ -8,9 +10,20 @@ import { Subject } from 'rxjs';
 })
 export class SessoesJulgamentosComponent implements OnInit {
 
-  constructor() { }
+  processos: Processo[];
+
+  constructor(
+    private _processoService: ProcessoService,
+  ) { }
 
   ngOnInit(): void {
+      this.processos = [];
+      this._processoService.listarProcessos().subscribe({
+        next: (data) => {
+          this.processos = data;
+          console.table(data);
+        }
+      });
   }
 
   eventsSubject: Subject<any> = new Subject<any>();
