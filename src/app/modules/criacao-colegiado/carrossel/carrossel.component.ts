87fdb6aa98fd26c3f429202be9c9622654723f03
ErickMVdO <erickmelovidal@gmail.com>
2021-08-28 18:33:25 -0300

    ✨ adicionando scroll do carrossel com as setas

--- src/app/modules/criacao-colegiado/carrossel/carrossel.component.ts
+++ src/app/modules/criacao-colegiado/carrossel/carrossel.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-carrossel',
   templateUrl: './carrossel.component.html',
-  styleUrls: ['./carrossel.component.scss']
+  styleUrls: ['./carrossel.component.scss'],
+  host: {
+    '(document:keydown)': 'handleKeyboardEvent($event)'
+  }
 })
 
 export class CarrosselComponent implements OnInit {
@@ -12,23 +15,33 @@ export class CarrosselComponent implements OnInit {
   @Input() hasIcon: boolean = false;
   @Input() class: string[] = [];
   @Input() idChip:string;
+  @Input() lastId:string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if(event.key=="ArrowRight") this.scrollRight(this.lastId);
+    else if(event.key=="ArrowLeft") this.scrollLeft(this.lastId);
+  }
+
   /**
    * 
    * @param id o id do elemento que eu quero fazer o scrollLeft
    */
-   public scrollLeft(id): void {
-    let el = document.getElementById(id);
-    el.scrollTo({ left: (el.scrollLeft - 150), behavior: 'smooth' });
+  public scrollLeft(id): void {
+    if(id){
+      let el = document.getElementById(id);
+      el.scrollTo({ left: (el.scrollLeft - 150), behavior: 'smooth' });
+    }
   }
-  public scrollRight(id): void {
-    let el = document.getElementById(id);
-    document.getElementById(id).scrollTo({ left: (el.scrollLeft + 150), behavior: 'smooth' });
+  public scrollRight(id: string): void {
+    if(id){
+      let el = document.getElementById(id);
+      document.getElementById(id).scrollTo({ left: (el.scrollLeft + 150), behavior: 'smooth' });
+    }
   }
 
 }
--- src/app/modules/criacao-colegiado/criacao-colegiado.component.ts
+++ src/app/modules/criacao-colegiado/criacao-colegiado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Colegiado, ComposicaoColegiado } from '../acervo/model/interfaces/colegiado.interface';
@@ -11,7 +11,7 @@ import { ProcessoService } from '../services/processo.service';
 @Component({
   selector: 'app-criacao-colegiado',
   templateUrl: './criacao-colegiado.component.html',
-  styleUrls: ['./criacao-colegiado.component.scss']
+  styleUrls: ['./criacao-colegiado.component.scss'],
 })
 export class CriacaoColegiadoComponent implements OnInit {
 
@@ -31,6 +31,7 @@ export class CriacaoColegiadoComponent implements OnInit {
   relator: Ministro;
   tags: string[] = [];
   documentos: string[] = [];
+  lastId: string = "idTags";
 
   post: {
     processo: '',
@@ -174,4 +175,9 @@ export class CriacaoColegiadoComponent implements OnInit {
       });
     }
   }
+
+  setId(id: string){
+    this.lastId = id;
+  }
+
 }
