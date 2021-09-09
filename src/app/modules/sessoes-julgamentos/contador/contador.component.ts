1735139d9557a15f967f35d7cbb0e7b127a9c491
Caio de Oliveira Mendes <caioom00@gmail.com>
2021-09-09 17:54:13 -0300

    feat: create counter component

---
+++ src/app/modules/sessoes-julgamentos/contador/contador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.scss']
+})
+export class ContadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
