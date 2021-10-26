4d60b01607999a62ab33041746fd3442db365288
Caio Mendes <caioom00@gmail.com>
2021-10-26 14:23:39 -0300

    adicionar iframe

--- src/app/modules/minhas-tarefas/minhas-tarefas.component.ts
+++ src/app/modules/minhas-tarefas/minhas-tarefas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-minhas-tarefas',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./minhas-tarefas.component.scss']
 })
 export class MinhasTarefasComponent implements OnInit {
+  url: SafeResourceUrl = '';
+  urlSelecionada: boolean = false;
 
-  constructor() { }
+  constructor(
+    private _sanitizer: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
+    this.url = this._sanitizer.bypassSecurityTrustResourceUrl('');
+  }
+
+  mostrarPagina(): void {
+      this.urlSelecionada = true;
+      this.url = this._sanitizer.bypassSecurityTrustResourceUrl('http://localhost:4200/criacao-colegiado?processo=ADI100-Ag-Ag-Ag&data=2016-08-29T09%253A12%253A33.001Z&colegiado=pleno&sessao=1000-2021');
+  }
+
+  mostrarOutraPagina(): void {
+    this.urlSelecionada = true;
+    this.url = this._sanitizer.bypassSecurityTrustResourceUrl('/acervo');
   }
 
 }
