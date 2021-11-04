9cce4faa18a43e2bc0d3327cf5f0a155d38d0fd2
douglasmonteles <silvadouglas328@gmail.com>
2021-11-04 12:55:06 -0300

    :sparkles: Craindo a interface e o mock de Dispositivos

---
+++ src/app/mock-api/pautas/dispositivo/data.ts
@@ -0,0 +1,8 @@
+import { Dispositivo } from "app/modules/acervo/model/interfaces/dispositivo.interface";
+
+export const dispositivos: Dispositivo[] = [
+  {
+    descricao: 'Procedente',
+  },
+];
+  
