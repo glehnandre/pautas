3bd50db5da0aced79056a96aa5e3751b686c93bd
Caio Mendes <caioom00@gmail.com>
2021-11-05 17:16:12 -0300

    Adicionar componente do relator

--- src/app/modules/revisar-inteiro-teor/revisar-inteiro-teor.module.ts
+++ src/app/modules/revisar-inteiro-teor/revisar-inteiro-teor.module.ts
@@ -3,6 +3,7 @@ import { Route, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RevisarInteiroTeorComponent } from './revisar-inteiro-teor.component';
+import { SharedModule } from '../../shared/shared.module';
 
 const revisarInteiroTeorRoutes: Route[] = [
     {
@@ -19,6 +20,7 @@ const revisarInteiroTeorRoutes: Route[] = [
   imports: [
     CommonModule,
     MatButtonModule,
+    SharedModule,
     RouterModule.forChild(revisarInteiroTeorRoutes),
   ]
 })
