ec32772eb8787ffb8baddba34cb5a61fc58d461b
ErickMVdO <erickmelovidal@gmail.com>
2021-11-11 18:48:44 -0300

    🎨 adicionando descricao do filtro em baixo do nome

--- src/app/modules/publicacoes/filtros/filtros.component.ts
+++ src/app/modules/publicacoes/filtros/filtros.component.ts
@@ -1,25 +1,29 @@
-import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { InformacoesDto } from 'app/modules/acervo/model/interfaces/informacoesDto.interface';
 
+interface Filtros {
+  agregacao: InformacoesDto;
+  selecionados: string[];
+}
 
 @Component({
   selector: 'app-filtros',
   templateUrl: './filtros.component.html',
   styleUrls: ['./filtros.component.scss']
 })
-export class FiltrosComponent implements OnInit {
+export class FiltrosComponent implements OnInit{
 
   @Input() agregacoes: InformacoesDto[] = [];
   @Input() pesquisas: string[] = [];
   @Output() removido = new EventEmitter<any>();
   @Output() emiteFiltros = new EventEmitter<any>();
 
+  filtros: Filtros[] = [];
   data_inicio: Date = new Date();
   data_fim: Date = new Date();
 
-  filtros: string[] = [];
+  filtrados: string[] = [];
 
   constructor() { }
 
@@ -33,13 +37,38 @@ export class FiltrosComponent implements OnInit {
   }
 
   atualizaFiltros(status: MatCheckboxChange){
-   if(status.checked) this.filtros.push(status.source.name);
-   else this.filtros.splice(this.filtros.indexOf(status.source.name), 1);
+    const name = status.source.name;
+    if(status.checked){
+      this.filtrados.push(name);
+
+      this.filtros.forEach(filtro=>{
+        if(filtro.agregacao.itens.find(item=>item.descricao==name))
+          filtro.selecionados.push(name);
+      })
+    }
+    else{
+      this.filtrados.splice(this.filtrados.indexOf(name), 1);
+
+      this.filtros.forEach(filtro=>{
+        if(filtro.agregacao.itens.find(item=>item.descricao==name))
+          filtro.selecionados.splice(filtro.selecionados.indexOf(name), 1);
+      })
+    } 
   }
 
   filtrar(){
-    this.emiteFiltros.emit(this.filtros);
+    this.emiteFiltros.emit(this.filtrados);
   }
   
+  recuperaFiltros(): Filtros[]{
+    if(this.filtros.length<this.agregacoes.length)
+    this.agregacoes.forEach(agregacao=>{
+      this.filtros.push({
+        agregacao: agregacao,
+        selecionados: []
+      })
+    })
+    return this.filtros;
+  }
 
 }
