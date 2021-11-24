9ed805b9f9133ea9173e2662da3fe26a5aef932e
douglasmonteles <silvadouglas328@gmail.com>
2021-11-24 18:21:33 -0200

    :sparkles: Salvando modelo de decisao via POST no mock api

--- src/app/modules/resultado-julgamento/form-modelo-decisao/form-modelo-decisao.component.ts
+++ src/app/modules/resultado-julgamento/form-modelo-decisao/form-modelo-decisao.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ResultadoJulgamentoService } from 'app/modules/services/resultado-julgamento.service';
 
 @Component({
   selector: 'app-form-modelo-decisao',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormModeloDecisaoComponent implements OnInit {
 
-  constructor() { }
+  formModeloDecisao: FormGroup;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _resultadoJulgamento: ResultadoJulgamentoService,
+    public dialogRef: MatDialogRef<FormModeloDecisaoComponent>,
+  ) { 
+    this.formModeloDecisao = this._fb.group({
+      classe:           ['', Validators.required],
+      tipo_capitulo:    ['', Validators.required],
+      recurso:          ['', Validators.required],
+      texto:            ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  public salvarModeloDecisao(): void {
+    if (this.formModeloDecisao.valid) {
+      this._resultadoJulgamento.salvarModeloDecisao(this.formModeloDecisao.value).subscribe({
+        next: () => {
+          this.dialogRef.close();
+        }
+      });
+    }
   }
 
 }
