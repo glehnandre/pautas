cc3f42be3122d71986ead2e6d7e2f187dc92fad1
ErickMVdO <erickmelovidal@gmail.com>
2021-12-03 18:51:46 -0300

    🎨 tratando presentes e ausentes

--- src/app/modules/finalizar-sessao-julgamento/composicao/composicao.component.ts
+++ src/app/modules/finalizar-sessao-julgamento/composicao/composicao.component.ts
@@ -21,18 +21,15 @@ export class ComposicaoComponent implements OnInit {
     private _ministroService: MinistroService,
   ) { }
 
-
   @Input() presidente: Ministro;
 
   presienteChecked: presienteChecked[] = [];
-  ministros: Ministro[];
   primeiraTurma: Ministro[];
   segundaTurma: Ministro[];
+  presentes: Ministro[] = [];
+  ausentes: Ministro[] = [];
 
   ngOnInit(): void {
-    this._ministroService.listarMinistros().subscribe(ministros=>{
-      this.ministros = ministros;
-    });
     this._ministroService.listarMinistrosDoColegiado('primeira-turma').subscribe(ministros=>{
       this.primeiraTurma = ministros;
     })
@@ -48,10 +45,66 @@ export class ComposicaoComponent implements OnInit {
     return false;
   }
 
-  checked(event: MatCheckboxChange){
+  check(event: MatCheckboxChange){
     this.presienteChecked.forEach(presidente=>{
       if(event.source.name == presidente.nome) presidente.checked = event.checked;
       else presidente.checked = false;
     })
+  }
+
+  atualizaPresentes(event: MatCheckboxChange, colegiado: string){
+    const nome = event.source.name;
+
+    if(colegiado=="presidente"){
+      if(event.checked){
+        this.presentes.push(this.presidente);
+        this.ausentes.splice(this.ausentes.indexOf(this.presidente), 1);
+      }
+      else this.presentes.splice(this.presentes.indexOf(this.presidente), 1);
+    }
+
+    else if(colegiado=="primeira-turma"){
+      if(event.checked){
+        this.presentes.push(this.primeiraTurma.find(ministro=>ministro.nome==nome));
+        this.ausentes.splice(this.ausentes.indexOf(this.primeiraTurma.find(ministro=>ministro.nome==nome)), 1);
+      }
+      else this.presentes.splice(this.presentes.indexOf(this.presentes.find(ministro=>ministro.nome==nome)), 1)
+    }
+
+    else if(colegiado=="segunda-turma"){
+      if(event.checked){
+        this.presentes.push(this.segundaTurma.find(ministro=>ministro.nome==nome));
+        this.ausentes.splice(this.ausentes.indexOf(this.segundaTurma.find(ministro=>ministro.nome==nome)), 1);
+      }
+      else this.presentes.splice(this.presentes.indexOf(this.presentes.find(ministro=>ministro.nome==nome)), 1)
+    }
+  }
+
+  atualizaAusentes(event: MatCheckboxChange, colegiado: string){
+    const nome = event.source.name;
+
+    if(colegiado=="presidente"){
+      if(event.checked){
+        this.ausentes.push(this.presidente);
+        this.presentes.splice(this.presentes.indexOf(this.presidente), 1);
+      }
+      else this.ausentes.splice(this.ausentes.indexOf(this.presidente), 1);
+    }
+
+    else if(colegiado=="primeira-turma"){
+      if(event.checked){
+        this.ausentes.push(this.primeiraTurma.find(ministro=>ministro.nome==nome));
+        this.presentes.splice(this.presentes.indexOf(this.primeiraTurma.find(ministro=>ministro.nome==nome)), 1);
+      }
+      else this.ausentes.splice(this.ausentes.indexOf(this.ausentes.find(ministro=>ministro.nome==nome)), 1)
+    }
+
+    else if(colegiado=="segunda-turma"){
+      if(event.checked){
+        this.ausentes.push(this.segundaTurma.find(ministro=>ministro.nome==nome));
+        this.presentes.splice(this.presentes.indexOf(this.segundaTurma.find(ministro=>ministro.nome==nome)), 1);
+      }
+      else this.ausentes.splice(this.ausentes.indexOf(this.ausentes.find(ministro=>ministro.nome==nome)), 1)
+    }
   }
 }
