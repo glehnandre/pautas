007ab9e65fd4a4aa9b64aeef132eaeab5c5b343f
Caio Mendes <caioom00@gmail.com>
2021-12-06 16:48:45 -0200

    Criar estrutura de adicionar novos documentos

---
+++ src/app/modules/revisar-inteiro-teor/acoes/incluir-documento/incluir-documento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incluir-documento',
+  templateUrl: './incluir-documento.component.html',
+  styleUrls: ['./incluir-documento.component.scss']
+})
+export class IncluirDocumentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
