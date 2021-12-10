cf611d9173d0994f24c77f3474aeafd5f696410a
douglasmonteles <silvadouglas328@gmail.com>
2021-12-10 15:45:24 -0200

    :sparkles: SDJCPAUTAS-221: Criando dinâmicamente o controle de formulário

--- src/app/modules/resultado-julgamento/form-indicacao-impedimentos/form-indicacao-impedimentos.component.ts
+++ src/app/modules/resultado-julgamento/form-indicacao-impedimentos/form-indicacao-impedimentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Ministro } from 'app/modules/acervo/model/interfaces/ministro.interface';
 import { MinistroService } from 'app/modules/services/ministro.service';
 import { Observable } from 'rxjs';
@@ -10,14 +11,48 @@ import { Observable } from 'rxjs';
 })
 export class FormIndicacaoImpedimentosComponent implements OnInit {
 
-  ministros$: Observable<Ministro[]>;
+  formIndicacao: FormGroup;
+  ministros: Ministro[] = [];
 
   constructor(
     private _ministroService: MinistroService,
-  ) { }
+    private _fb: FormBuilder,
+  ) { 
+    this._construirFormulario(); // Garante que uma instância de FormGroup vazia seja criada durante a criação do componente
+
+    this._ministroService.listarMinistros().subscribe({
+      next: (ministros) => {
+        this.ministros = ministros;
+        this._construirFormulario();
+      }
+    });
+  }
 
   ngOnInit(): void {
-    this.ministros$ = this._ministroService.listarMinistros();
+    
+  }
+
+  public salvar(): void {
+    if (this.formIndicacao.valid) {
+      console.log(this.formIndicacao.value)
+    }
+  }
+
+  /**
+   * @private Método privado
+   * @description Cria uma instância de FormGroup cujo os atributos do objeto 
+   *              são criados de forma dinâmica, neste caso, o formControlName
+   *              será o id de cada ministro.
+   * @author Douglas da Silva Monteles
+   */
+  private _construirFormulario(): void {
+    let formMinistros = {};
+
+    this.ministros.forEach(({id}) => {
+      formMinistros[`${id}`] = [null, Validators.required];
+    });
+
+    this.formIndicacao = this._fb.group(formMinistros);
   }
 
 }
