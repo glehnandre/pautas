a88c22fe51cf992bb50c3dafde1560c3d63142fc
Caio Mendes <caioom00@gmail.com>
2022-02-02 17:53:12 -0200

    Refatorar função de recuperar data

--- src/app/modules/revisar-inteiro-teor/conteudo-publicacao/conteudo-publicacao.component.ts
+++ src/app/modules/revisar-inteiro-teor/conteudo-publicacao/conteudo-publicacao.component.ts
@@ -93,13 +93,19 @@ export class ConteudoPublicacaoComponent implements OnInit, AfterViewInit {
    */
   getData(isoDate: string = new Date().toISOString(), firstDate: boolean = true): string{
     const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
-    const datepipe: DatePipe = new DatePipe('pt-BR')
-    let newDate = new Date(isoDate);
-    let data: string;
-    (firstDate) ? data = String(newDate.getDate()).padStart(2, '0')+'/'+meses[newDate.getMonth()]+'/'+newDate.getFullYear().toString().slice(2,4)
-                : data = datepipe.transform(isoDate, "dd/MM/YYYY hh:mm");
-    console.log(data);
-    return data;
+
+    const datepipe: DatePipe = new DatePipe('pt-BR');
+
+    const novaData = new Date(isoDate);
+
+    const diaFormatado = String(novaData.getDate()).padStart(2, '0');
+    const mesFormatado = meses[novaData.getMonth()];
+    const anoFormatado = novaData.getFullYear().toString().slice(2,4);
+
+    const dataFormatada = firstDate ? `${diaFormatado}/${mesFormatado}/${anoFormatado}`
+                                    : datepipe.transform(isoDate, "dd/MM/YYYY hh:mm");
+
+    return dataFormatada;
   }
 
   /**
