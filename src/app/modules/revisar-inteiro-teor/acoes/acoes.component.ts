982e1954566904208f6d75b841d97895defd3749
Caio Mendes <caioom00@gmail.com>
2022-02-04 17:40:53 -0200

    Adicionar ações em menu responsivo no Inteiro Teor

--- src/app/modules/revisar-inteiro-teor/acoes/acoes.component.ts
+++ src/app/modules/revisar-inteiro-teor/acoes/acoes.component.ts
@@ -8,6 +8,7 @@ import { Documento } from '../../acervo/model/interfaces/documento.interface';
 import { publicacao } from '../../../mock-api/pautas/publicacoes/data';
 import { RevisarInteiroTeorService } from '../../services/revisar-inteiro-teor.service';
 import { IncluirDocumentoComponent } from './incluir-documento/incluir-documento.component';
+import { MatSelectChange } from '@angular/material/select';
 
 export interface RevisaoInteiroTeor {
     id_processo: number;
@@ -44,6 +45,25 @@ export class AcoesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  public selecionarAcaoCorrespondente(event: MatSelectChange): void {
+    const { value } = event;
+
+    switch (value) {
+        case 'visualizar':
+            this.visualizarInteiroTeor();
+            break;
+        case 'incluir':
+            this.incluirNovoDocumento();
+            break;
+        case 'remover':
+            this.removerInteiroTeor();
+            break;
+        case 'revisar':
+            this.revisar();
+            break;
+    }
   }
 
   public visualizarInteiroTeor(): void {
