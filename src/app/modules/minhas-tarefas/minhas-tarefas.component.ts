9ffef760705677ab7008ba86e9999b1ed59d1993
douglasmonteles <silvadouglas328@gmail.com>
2022-02-07 13:50:55 -0200

    :sparkles: SDJCPAUTAS-243: Adicionando alerta

--- src/app/modules/minhas-tarefas/minhas-tarefas.component.ts
+++ src/app/modules/minhas-tarefas/minhas-tarefas.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { FuseAlertService } from '@fuse/components/alert';
 import { ITask } from '../acervo/model/interfaces/itask.interface';
 import { FormComentarioComponent } from './form-comentario/form-comentario.component';
 
@@ -13,9 +14,14 @@ export class MinhasTarefasComponent implements OnInit {
     panelOpenState: boolean = false;
     tarefas: ITask[] = [];
     tarefasSelecionadas: ITask[] = [];
+    alerta: { titulo: string; mensagem: string } = null;
+    timeout = null;
+
+    readonly NOME_DO_ALERTA = 'minhas-tarefas-alert';
 
     constructor(
         private _dialog: MatDialog,
+        private _fuseAlertService: FuseAlertService,
     ) {}
 
     ngOnInit(): void {
@@ -32,7 +38,7 @@ export class MinhasTarefasComponent implements OnInit {
     }
 
     public abrirModalDeIncluirComentario(): void {
-        if (!this._isTarefasSelecionadasVazia()) {
+        if (!this._isTarefasSelecionadasVazia() && !this._isLimiteDeSelecaoAtingido(1)) {
             const dialogRef = this._dialog.open(FormComentarioComponent, {
                 data: {
                     tarefas: [...this.tarefasSelecionadas],
@@ -43,6 +49,34 @@ export class MinhasTarefasComponent implements OnInit {
 
     private _isTarefasSelecionadasVazia(): boolean {
         return this.tarefasSelecionadas.length === 0;
+    }
+
+    private _isLimiteDeSelecaoAtingido(limite?: number) {
+        if (limite) {
+            if (this.tarefasSelecionadas.length > limite) {   
+                this.alerta = {
+                    titulo: 'Limite atingido',
+                    mensagem: `Para executar essa funcionalidade, selecione no máximo ${limite} tarefa(as).`,
+                };
+                
+                this._exibirAlerta();
+                return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+
+    private _exibirAlerta(): void {
+        this._fuseAlertService.show(this.NOME_DO_ALERTA);
+
+        clearTimeout(this.timeout);
+    
+        this.timeout = setTimeout(() => {
+            this._fuseAlertService.dismiss(this.NOME_DO_ALERTA);
+            this.alerta = null;
+        }, 6000);
     }
 
 }
